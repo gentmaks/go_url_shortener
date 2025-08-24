39c403892e35cc2b054c95cd607d9ffdf03aebc0
Gent <gmaksu27@colby.edu>
2025-08-24 12:04:12 -0700

    finalizing redirect logic

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 import "github.com/gin-gonic/gin"
+import "github.com/gentmaks/go_url_shortener/handler"
+import "github.com/gentmaks/go_url_shortener/store"
 
 func main() {
 
@@ -12,6 +14,16 @@ func main() {
     })
   })
   
+  r.POST("/create-short-url", func(c *gin.Context) {
+    handler.CreateShortUrl(c)
+  })
+  
+  r.GET("/:shortUrl", func(c *gin.Context) {
+    handler.HandleShortUrlRedirect(c)
+  })
+
+  store.InitializeStore()
+
   err := r.Run(":9808")
   if err != nil {
     panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
